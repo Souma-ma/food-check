ce710829964d1a9d2e16d0eb608cd8c8fd9faeaa
Souma-ma <somamatsutani07@gmail.com>
2021-02-11 11:53:17 +0900

    バリデーションとアソシエーションを作成

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,19 @@
 class Food < ApplicationRecord
+
+  with_options presense: true do
+    validates :name
+    validates :calorie
+    validates :protein
+    validates :fat
+    validates :carbohydrate
+  end
+
+  with_options format: {with: /\A[0-9]+\z/} do
+    validates :calorie
+    validates :protein
+    validates :fat
+    validates :carbohydrate
+  end
+
+  has_one_attached :image
 end
