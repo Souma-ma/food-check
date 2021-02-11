48332095ce8447e190519fb41364ddee6214d60b
Souma-ma <somamatsutani07@gmail.com>
2021-02-11 19:34:33 +0900

    foodモデル単体テストの雛形を作成

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @food = FactoryBot.build(:food)
+  end
+
+  describe "食材投稿機能" do
+    context "食材投稿ができるとき" do
+      it "全ての値を正しく入力すれば保存できる" do
+        
+      end
+    end
+    context "食材投稿ができないとき" do
+      it "nameが空では保存できない" do
+        
+      end
+      it "calorieが空では保存できない" do
+        
+      end
+      it "calorieが半角数字以外では保存できない" do
+        
+      end
+      it "proteinが空では保存できない" do
+        
+      end
+      it "proteinが半角数字以外では保存できない" do
+        
+      end
+      it "fatが空では保存できない" do
+        
+      end
+      it "fatが半角数字以外では保存できない" do
+        
+      end
+      it "carbohydrateが空では保存できない" do
+        
+      end
+      it "carbohydrateが半角数字以外では保存できない" do
+        
+      end
+      it "imageが空では保存できない" do
+        
+      end
+    end
+  end
 end
