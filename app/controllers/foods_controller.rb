fcd93604515bed8fdbff382cee124c54fed6cd80
Souma-ma <somamatsutani07@gmail.com>
2021-02-11 17:56:42 +0900

    createアクションとストロングパラメーターを作成

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,5 +7,23 @@ class FoodsController < ApplicationController
   end
 
   def create
+    @food = Food.new(food_params)
+    if @food.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  
+  
+  private
+
+  def food_params
+    params.require(:food).permit(:image, :name, :calorie, :protein, :fat, :carbohydrate)
+  end 
+  
+  
+  
 end
+
