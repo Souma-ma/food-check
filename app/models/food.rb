499490c18438ec66172fd6c2639c002a6e8e43ae
Souma-ma <somamatsutani07@gmail.com>
2021-02-16 11:53:52 +0900

    検索機能を実装

--- app/models/food.rb
+++ app/models/food.rb
@@ -17,4 +17,12 @@ class Food < ApplicationRecord
   end
 
   has_one_attached :image
+
+  def self.search(search)
+    if search != ""
+      Food.where('name LIKE(?)', "%#{search}%")
+    else
+      Food.all
+    end
+  end
 end
