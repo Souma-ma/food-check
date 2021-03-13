3bdf40f2db148e8d3b33a6b333d6a5ca8a152d77
Souma-ma <somamatsutani07@gmail.com>
2021-03-13 21:50:17 +0900

    users#showを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  
+  #お気に入りを取得する処理
+  def show
+    favorites = Favorite.where(user_id: current_user.id).pluck(:food_id)
+    @foods = Food.find(favorites)
+  end
 end
