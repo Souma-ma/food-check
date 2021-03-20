d36f048b26f06d834a4d7395bc50237682876339
Souma-ma <somamatsutani07@gmail.com>
2021-03-20 16:15:22 +0900

    ユーザー登録の結合テスト

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :food do
     name         {Faker::Food.description}
     calorie      {100}
-    protein      {50}
-    fat          {20}
-    carbohydrate {200}
+    protein      {50.0}
+    fat          {20.0}
+    carbohydrate {200.0}
 
     after(:build) do |food|
       food.image.attach(io: File.open('public/images/test-img.jpg'), filename: 'test-img.jpg')
---
+++ spec/system/users_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "ユーザー新規登録", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー新規登録ができる時' do
+    
+  end
+  context 'ユーザー新規登録ができない時' do
+    
+  end
+end
+
+RSpec.describe "ログイン", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ログインができる時' do
+    
+  end
+  context 'ログインができない時' do
+    
+  end
+end
