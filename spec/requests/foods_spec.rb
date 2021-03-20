b553dfa4c3b06902f6068c3eb977728b95393da4
Souma-ma <somamatsutani07@gmail.com>
2021-03-20 14:43:09 +0900

    foodsコントローラのテストexampleを作成

---
+++ spec/requests/foods_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe "Foods", type: :request do
+
+  before do
+    @food = FactoryBot.create(:food)
+  end
+
+  describe "GET #index" do
+    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
+      
+    end
+    it "indexアクションにリクエストするとレスポンスに食材の画像が存在する" do
+      
+    end
+    it "indexアクションにリクエストするとレスポンスに食材名が存在する" do
+      
+    end
+    it "indexアクションにリクエストするとレスポンスに食材のカロリー量が存在する" do
+      
+    end
+    it "indexアクションにリクエストするとレスポンスに食材のたんぱく質量が存在する" do
+      
+    end
+    it "indexアクションにリクエストするとレスポンスに食材の脂質量が存在する" do
+      
+    end
+    it "indexアクションにリクエストするとレスポンスに食材の炭水化物量が存在する" do
+      
+    end
+    it "indexアクションにリクエストするとレスポンスに食材検索フォームが存在する" do
+      
+    end
+  end
+
+  describe "GET #show" do
+    it "showアクションにリクエストすると正常にレスポンスが返ってくる" do
+      
+    end
+    it "showアクションにリクエストするとレスポンスに食材の画像が存在する" do
+      
+    end
+    it "showアクションにリクエストするとレスポンスに食材名が存在する" do
+      
+    end
+    it "showアクションにリクエストするとレスポンスに食材のカロリー量が存在する" do
+      
+    end
+    it "showアクションにリクエストするとレスポンスに食材のたんぱく質量が存在する" do
+      
+    end
+    it "showアクションにリクエストするとレスポンスに食材の脂質量が存在する" do
+      
+    end
+    it "showアクションにリクエストするとレスポンスに食材の炭水化物量が存在する" do
+      
+    end
+  end
+end
