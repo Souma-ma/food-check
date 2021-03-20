d9bddedd8cf45d15478df684e7bf11993386a222
Souma-ma <somamatsutani07@gmail.com>
2021-03-20 17:14:54 +0900

    ユーザー新規登録結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -7,12 +7,50 @@ RSpec.describe "ユーザー新規登録", type: :system do
 
   context 'ユーザー新規登録ができる時' do
     it '正しい情報を入力するとユーザー新規登録ができてトップページに移動する' do
-      
+      # トップページに移動する
+       visit root_path
+      # トップページに新規登録ページに遷移するボタンがある
+       expect(page).to have_content('新規登録')
+      # 新規登録ページに移動する
+       visit new_user_registration_path
+      # ユーザー情報を入力する
+       fill_in "user[nickname]" , with: @user.nickname
+       fill_in 'user[email]', with: @user.email
+       fill_in 'user[password]', with: @user.password
+       fill_in 'user[password_confirmation]', with: @user.password_confirmation
+      # 登録するボタンを押すとユーザーモデルのカウントが1上がる
+       expect {
+         find('input[name="commit"]').click
+       }.to change { User.count }.by(1)
+      # トップページに遷移する
+       expect(current_path).to eq(root_path) 
+      # 新規登録とログインボタンは表示されない
+       expect(page).to have_no_content('新規登録')
+       expect(page).to have_no_content('ログイン')
+      # ログアウトボタンとマイページボタンが表示されている
+       expect(page).to have_content('ログアウト')
+       expect(page).to have_content('マイページ')
     end
   end
   context 'ユーザー新規登録ができない時' do
     it '必須項目を入力しないとユーザー新規登録ができず新規登録画面に戻ってくる' do
-      
+      # トップページに移動する
+       visit root_path
+      # トップページに新規登録ページに遷移するボタンがある
+       expect(page).to have_content('新規登録')
+      # 新規登録ページに遷移する
+       visit new_user_registration_path
+      # ユーザー情報を入力する
+       fill_in "user[nickname]", with: ""
+       fill_in "user[email]", with: ""
+       fill_in "user[password]", with: ""
+       fill_in "user[password_confirmation]", with: ""
+      # 登録するボタンを押してもユーザーモデルのカウントは上がらない
+       expect {
+         find('input[name="commit"]').click
+       }.to change { User.count }.by(0)
+      # 新規登録ページに戻される
+       expect(current_path).to eq "/users"
     end
   end
 end
@@ -24,7 +62,6 @@ RSpec.describe "ログイン", type: :system do
 
   context 'ログインができる時' do
     it '保存されているユーザーの情報と合致すればログインできる' do
-      
     end
   end
   context 'ログインができない時' do
