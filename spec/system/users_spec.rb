82f6b6b686ca2add2abf9b3965b298aafbcc95d8
Souma-ma <somamatsutani07@gmail.com>
2021-03-20 16:19:44 +0900

    新規登録、ログインのテストのexampleを作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -6,10 +6,14 @@ RSpec.describe "ユーザー新規登録", type: :system do
   end
 
   context 'ユーザー新規登録ができる時' do
-    
+    it '正しい情報を入力するとユーザー新規登録ができてトップページに移動する' do
+      
+    end
   end
   context 'ユーザー新規登録ができない時' do
-    
+    it '必須項目を入力しないとユーザー新規登録ができず新規登録画面に戻ってくる' do
+      
+    end
   end
 end
 
@@ -19,9 +23,13 @@ RSpec.describe "ログイン", type: :system do
   end
 
   context 'ログインができる時' do
-    
+    it '保存されているユーザーの情報と合致すればログインできる' do
+      
+    end
   end
   context 'ログインができない時' do
-    
+    it '保存されているユーザーの情報と合致しないとログインできない' do
+      
+    end
   end
 end
